7f45b1640f6cfcb6463f47872c27bf60cdce5b92
jomora24 <jomora24@gmail.com>
2018-09-05 02:37:32 -0400

    Navegation Drawer

---
+++ app/src/main/java/com/jordy/mapclient/Fragments/PerfilFragment.java
@@ -0,0 +1,19 @@
+package com.jordy.mapclient.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jordy.mapclient.R;
+
+public class PerfilFragment extends Fragment{
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_perfil,container,false);
+        return rootView;
+    }
+
+}
